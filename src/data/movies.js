2091477c4dee32b50cf5781164eb81c86b873593
Jyoti Rani <jyoti.purav09@gmail.com>
2025-12-23 21:15:52 +0530

    my project

---
+++ src/data/movies.js
@@ -0,0 +1,115 @@
+export const movies = [
+    { id: 1, name: "Inception", poster: "https://i.pinimg.com/1200x/8e/00/ef/8e00ef29a3cdfff5bb1887a5a712fac9.jpg" },
+    { id: 2, name: "Interstellar", poster: "https://i.pinimg.com/736x/3e/1f/66/3e1f66f76ffe17e6cb6f30a2c08a6c3d.jpg" },
+    { id: 3, name: "3 Idiots", poster: "https://i.pinimg.com/736x/5f/fb/49/5ffb496fb8b003fed1d27010cb815d53.jpg" },
+ 
+    { id: 4, name: "Maharaja", poster: "https://i.pinimg.com/1200x/57/78/6d/57786dc7d3c8ebce56cefd1d29748de2.jpg" },
+    { id: 5, name: "Life", poster: "https://i.pinimg.com/1200x/de/13/2e/de132e68791cef80562c04fe2c5f5612.jpg" },
+    { id: 6, name: "The Martian", poster: "https://i.pinimg.com/736x/33/fd/ed/33fded198d0df72de6bfeadc664de1a7.jpg" },
+ 
+    { id: 7, name: "Iron Man", poster: "https://i.pinimg.com/1200x/62/67/dd/6267ddd3515252c95cc61f8335ff2a31.jpg" },
+    { id: 8, name: "Iron Man 2", poster: "https://i.pinimg.com/736x/86/d6/61/86d66186696128e41f1cbbff647619d7.jpg" },
+    { id: 9, name: "Iron Man 3", poster: "https://i.pinimg.com/736x/43/12/2f/43122f1057f55bd715fbb509c0b466c9.jpg" },
+ 
+    { id: 10, name: "Captain America: Civil War", poster: "https://i.pinimg.com/1200x/ad/d9/94/add99468e27e0d60d21d2e360d547d6d.jpg" },
+ 
+    { id: 11, name: "Guardians of the Galaxy", poster: "https://i.pinimg.com/1200x/b1/8c/0e/b18c0e2290608e601608db19054d0acb.jpg" },
+    { id: 12, name: "Guardians of the Galaxy Vol. 2", poster: "https://i.pinimg.com/1200x/e4/ae/3d/e4ae3d25d7aa84112e39cdae5ce0d0bf.jpg" },
+    { id: 13, name: "Guardians of the Galaxy Vol. 3", poster: "https://i.pinimg.com/736x/fe/85/04/fe85046d2277641be067796caafcb6f1.jpg" },
+ 
+    { id: 14, name: "Thor: Ragnarok", poster: "https://i.pinimg.com/736x/f8/e5/4c/f8e54cec508d419b0467169f91099288.jpg" },
+ 
+    { id: 15, name: "The Avengers", poster: "https://i.pinimg.com/1200x/3c/b4/28/3cb428f7b5e7246ee9c2727862e423e4.jpg" },
+    { id: 16, name: "Avengers: Age of Ultron", poster: "https://i.pinimg.com/1200x/ff/a3/53/ffa35393a087e6015e6da5e34e7caa25.jpg" },
+    { id: 17, name: "Avengers: Infinity War", poster: "https://i.pinimg.com/736x/8f/3f/56/8f3f56010d29fbfcee42adc250122ef1.jpg" },
+    { id: 18, name: "Avengers: Endgame", poster: "https://i.pinimg.com/736x/29/27/d2/2927d271355daa9ab0a43acd95c0d17b.jpg" },
+ 
+    { id: 19, name: "Doctor Strange", poster: "https://i.pinimg.com/1200x/58/c4/30/58c43096916cebd2260e6a62f1ef91ad.jpg" },
+    { id: 20, name: "Shang-Chi and the Legend of the Ten Rings", poster: "https://i.pinimg.com/1200x/91/b3/1e/91b31e070a20658fd193b93cfe7d8a7f.jpg" },
+ 
+    { id: 21, name: "Stranger Things (Season 1)", poster: "https://i.pinimg.com/1200x/1a/70/0e/1a700ebd03486d7062bea768c1a26147.jpg" },
+    { id: 22, name: "Stranger Things (Season 2)", poster: "https://i.pinimg.com/1200x/39/97/6f/39976f4342680b18716158b555069df1.jpg" },
+    { id: 23, name: "Stranger Things (Season 3)", poster: "https://i.pinimg.com/736x/17/fe/69/17fe6912073485a03ac86229015aa1e4.jpg" },
+    { id: 24, name: "Stranger Things (Season 4)", poster: "https://i.pinimg.com/1200x/78/48/e1/7848e1a605f1b98a6f2f816ef9fb393e.jpg" },
+    { id: 25, name: "Stranger Things (Season 5)", poster: "https://i.pinimg.com/736x/46/10/04/4610046bb75ec6afd65fe43ed99c6a9f.jpg" },
+ 
+    { id: 26, name: "Spider-Man: Homecoming", poster: "https://i.pinimg.com/736x/16/92/6d/16926df862371d2a843caae1cda2d5b3.jpg" },
+    { id: 27, name: "Spider-Man: No Way Home", poster: "https://i.pinimg.com/736x/84/68/9c/84689c91ae13f9770c5de73f72576833.jpg" },
+ 
+    { id: 28, name: "Harry Potter and the Philosopher's Stone", poster: "https://i.pinimg.com/1200x/56/28/d8/5628d8ccd4ad79ef6eb8347ee8b77f7c.jpg" },
+    { id: 29, name: "Harry Potter and the Chamber of Secrets", poster: "https://i.pinimg.com/736x/36/4b/54/364b5433c456381e0305d1322c338fc3.jpg" },
+    { id: 30, name: "Harry Potter and the Prisoner of Azkaban", poster: "https://i.pinimg.com/736x/af/6f/ee/af6fee34d477d68a15d3cf2a0137c695.jpg" },
+    { id: 31, name: "Harry Potter and the Goblet of Fire", poster: "https://i.pinimg.com/736x/55/46/62/5546620133964f0b5b45e63db0a48806.jpg" },
+    { id: 32, name: "Harry Potter and the Order of the Phoenix", poster: "https://i.pinimg.com/736x/60/f2/cf/60f2cf169a1f7ca748aea8a5c49c415a.jpg" },
+    { id: 33, name: "Harry Potter and the Half-Blood Prince", poster: "https://i.pinimg.com/736x/f3/b3/a4/f3b3a4a691d48313fc0d49cb65315159.jpg" },
+    { id: 34, name: "Harry Potter and the Deathly Hallows – Part 1", poster: "https://i.pinimg.com/736x/e9/66/9b/e9669b3b0b394558e04f8752ed0daf48.jpg" },
+    { id: 35, name: "Harry Potter and the Deathly Hallows – Part 2", poster: "https://i.pinimg.com/736x/4a/24/fa/4a24fa06ad121911b2ecc64e821f9540.jpg" },
+ 
+    { id: 36, name: "X-Men", poster: "https://i.pinimg.com/736x/d5/38/6c/d5386cff02b97cc6da7db9bef46a5413.jpg" },
+    { id: 37, name: "X2: X-Men United", poster: "https://i.pinimg.com/736x/c1/f3/6a/c1f36a0ba8719c364024958ccedcef2e.jpg" },
+    { id: 38, name: "X-Men: The Last Stand", poster: "https://i.pinimg.com/1200x/49/21/fe/4921fe889ab1e1c1ea246e930a8d3518.jpg" },
+    { id: 39, name: "X-Men: Days of Future Past", poster: "https://i.pinimg.com/736x/ce/ab/10/ceab10fc4390345ec244b53ba59790fc.jpg" },
+    { id: 40, name: "X-Men: Apocalypse", poster: "https://i.pinimg.com/736x/4e/1d/98/4e1d9891c348a9ed5c2afc444b5f65c1.jpg" },
+    { id: 41, name: "X-Men: Dark Phoenix", poster: "https://i.pinimg.com/736x/7f/03/4d/7f034d416ef196289a464df10c2f8335.jpg" },
+ 
+    { id: 42, name: "Deadpool", poster: "https://i.pinimg.com/736x/30/41/58/3041585c51577893747691a2e36b791d.jpg" },
+    { id: 43, name: "Deadpool & Wolverine", poster: "https://i.pinimg.com/736x/40/96/3c/40963c4346d75c40ee0e1c8d7d07f705.jpg" },
+    { id: 44, name: "Ice Age", poster: "https://i.pinimg.com/736x/8c/42/d8/8c42d85e0a79b539925117004392abfc.jpg" },
+    { id: 45, name: "Ice Age: The Meltdown", poster: "https://i.pinimg.com/1200x/91/87/62/9187626e56d918e37ae52b4e58c38f7c.jpg" },
+    { id: 46, name: "Ice Age: Dawn of the Dinosaurs", poster: "https://i.pinimg.com/1200x/d6/f4/3c/d6f43c79877cd23201d1245bfd219da8.jpg" },
+    { id: 47, name: "Ice Age: Continental Drift", poster: "https://i.pinimg.com/1200x/a9/64/4c/a9644ce5ecdae1ceff42f8f931ce479a.jpg" },
+ 
+ 
+    { id: 48, name: "Stuart Little", poster: "https://i.pinimg.com/736x/ac/a5/09/aca509c98f2c204fa04027e22c2089fa.jpg" },
+ 
+    { id: 49, name: "Moonfall", poster: "https://i.pinimg.com/736x/63/17/0a/63170a3e05e90cd478dd97208703a1a8.jpg" },
+    { id: 50, name: "Passengers", poster: "https://i.pinimg.com/736x/ba/0c/c7/ba0cc77b5962c6a6c539f66dea088c20.jpg" },
+    { id: 51, name: "Salaar", poster: "https://i.pinimg.com/1200x/df/a2/71/dfa271537f2992a5dddade922fcbe6fd.jpg" },
+    { id: 52, name: "RRR", poster: "https://i.pinimg.com/736x/72/94/e8/7294e8061ab3a3af63d4b1a1bfbb97a3.jpg" },
+ 
+    { id: 53, name: "Saw", poster: "https://i.pinimg.com/736x/96/a5/e8/96a5e82c9aca8fd88756e193fa370ff9.jpg" },
+    { id: 54, name: "Saw II", poster: "https://i.pinimg.com/736x/83/0e/5c/830e5c9cad5c0ff40b297732e012628e.jpg" },
+    { id: 55, name: "Saw III", poster: "https://i.pinimg.com/1200x/8c/4b/9c/8c4b9c96780dd20c16062e3141e586e7.jpg" },
+    { id: 56, name: "Saw IV", poster: "https://i.pinimg.com/1200x/08/fc/35/08fc357c934829e8637d1b05e9a170a4.jpg" },
+    { id: 57, name: "Saw V", poster: "https://i.pinimg.com/736x/f4/ba/64/f4ba643ae661cb624c168b1e18c18e8f.jpg" },
+    { id: 58, name: "Saw VI", poster: "https://i.pinimg.com/736x/fa/96/da/fa96dab129c4e395697190ced436dc5f.jpg" },
+    { id: 59, name: "Saw 3D: The Final Chapter", poster: "https://i.pinimg.com/736x/eb/19/4f/eb194f4bc93aa824f8be5a08327f2e26.jpg" },
+    { id: 60, name: "Jigsaw", poster: "https://i.pinimg.com/1200x/02/3d/8c/023d8c88c22c7b5f8ff1933c97cd9e82.jpg" },
+    { id: 61, name: "Spiral: From the Book of Saw", poster: "https://i.pinimg.com/736x/19/c0/01/19c001bb35a3f9cbfb79924fcad5383d.jpg" },
+    { id: 62, name: "Saw X", poster: "https://i.pinimg.com/1200x/99/87/de/9987deb79b124ad771c7c02e69f57e95.jpg" },
+ 
+    { id: 63, name: "Loki (Web Series – Season 1)", poster: "https://i.pinimg.com/736x/88/9a/e5/889ae57e0739848970bd3297a6c39284.jpg" },
+    { id: 64, name: "Loki (Web Series – Season 2)", poster: "https://i.pinimg.com/1200x/77/b2/66/77b266316d120f4ac7bc9f694c80f623.jpg" },
+    { id: 65, name: "Moon Knight (Web Series – Season 1)", poster: "https://i.pinimg.com/736x/05/23/9c/05239c46d7417441d5d2f777929c2ce8.jpg" },
+ 
+    { id: 66, name: "Lost in Space (Web Series – Season 1)", poster: "https://i.pinimg.com/1200x/5c/2e/df/5c2edf3af0687652a3031c2c7337499e.jpg" },
+    { id: 67, name: "Lost in Space (Web Series – Season 2)", poster: "https://i.pinimg.com/736x/28/2b/e3/282be393ca96c425266880f277122f1b.jpg" },
+    { id: 68, name: "Lost in Space (Web Series – Season 3)", poster: "https://i.pinimg.com/736x/5f/9c/94/5f9c94a4a0c078506975c4809fbd854d.jpg" },
+ 
+    { id: 69, name: "The Boys (Web Series – Season 1)", poster: "https://i.pinimg.com/736x/ad/a0/f4/ada0f42071e082e39b763df643a00c63.jpg" },
+    { id: 70, name: "The Boys (Web Series – Season 2)", poster: "https://i.pinimg.com/1200x/34/c8/27/34c827b2ce7cdb8bf3c01fb1257b52e4.jpg" },
+    { id: 71, name: "The Boys (Web Series – Season 3)", poster: "https://i.pinimg.com/1200x/83/38/39/833839817bb36ac7bd2d47379622a2ca.jpg" },
+    { id: 72, name: "The Boys (Web Series – Season 4)", poster: "https://i.pinimg.com/1200x/4c/bb/e9/4cbbe9fcbcdf694bd8b419a8012853d4.jpg" },
+ 
+    { id: 73, name: "The 100 (Web Series – Season 1)", poster: "https://i.pinimg.com/736x/54/e3/a4/54e3a479bc5f92229a399e4cc13ed6da.jpg" },
+    { id: 74, name: "The 100 (Web Series – Season 2)", poster: "https://i.pinimg.com/736x/f7/fc/91/f7fc91800d4e87ffadfb215774dff746.jpg" },
+    { id: 75, name: "The 100 (Web Series – Season 3)", poster: "https://i.pinimg.com/1200x/f1/61/ae/f161ae27e78a3996a7f51153ef6cf4a5.jpg" },
+    { id: 76, name: "The 100 (Web Series – Season 4)", poster: "https://i.pinimg.com/736x/99/2e/05/992e059051de736da1a69b12ee5130e7.jpg" },
+    { id: 77, name: "The 100 (Web Series – Season 5)", poster: "https://i.pinimg.com/736x/61/f4/12/61f4127fff506e282e29b5cdcbda30f7.jpg" },
+    { id: 78, name: "The 100 (Web Series – Season 6)", poster: "https://i.pinimg.com/736x/21/56/a5/2156a5d5633b8334319dac2727c0b43e.jpg" },
+    { id: 79, name: "The 100 (Web Series – Season 7)", poster: "https://i.pinimg.com/736x/6d/70/5e/6d705e4e57e370320276553c25b3ea10.jpg" },
+ 
+    { id: 80, name: "LUCY", poster: "https://i.pinimg.com/736x/b4/f1/a8/b4f1a88ad6d3c79566a78923c16d1c41.jpg" },
+    { id: 81, name: "DARK (Web Series – Season 1)", poster: "https://i.pinimg.com/736x/fc/de/ca/fcdeca884756eca6b5f79726409fcb42.jpg" },
+    { id: 82, name: "DARK (Web Series – Season 2)", poster: "https://i.pinimg.com/736x/85/39/00/853900bfa08ce0b3f41a6776d252a574.jpg" },
+    { id: 83, name: "DARK (Web Series – Season 3)", poster: "https://i.pinimg.com/1200x/9a/75/93/9a7593e3e3689663c42c96ef70bd75be.jpg" },
+ 
+    { id: 84, name: "From (Web Series – Season 1)", poster: "https://m.media-amazon.com/images/M/MV5BYzM5ZWMxOGEtZjEyMC00YjQ0LThiYjEtZjVkZGEzN2NlOGEwXkEyXkFqcGc@._V1_FMjpg_UY1920_.jpg%20Request%20Method%20GET%22 "},
+    { id: 85, name: "From (Web Series – Season 2)", poster: "https://image.tmdb.org/t/p/original/kKUQPhx60p9zg3tSgaT8PJ1KAMR.jpg" },
+    { id: 86, name: "From (Web Series – Season 3)", poster: "https://image.tmdb.org/t/p/original/dIeamVg4smRMBG89HNycyxt21UH.jpg" },
+ 
+    { id: 87, name: "From (Season 4) (Coming Soon)", poster: "https://image.tmdb.org/t/p/original/qeOwFpnGeFYgKT1M4iCzgX0yRQZ.jpg" },
+    { id: 88, name: "Avengers: Doomsday (Coming Soon)", poster: "https://i.pinimg.com/736x/02/bc/ea/02bceadcb2880adc069745135d2eb293.jpg" },
+];
+ 
+ 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,43 +1,197 @@
-import { useState } from "react";
-export default function Login(){
-    const [email,setEmail]=useState("");
-    const [password,setPassword] =useState("");
-    return(
-        //full div screen
+import { useEffect, useState } from "react";
+import { movies } from "../data/Movies";
+ 
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+ 
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [previousIndex, setPreviousIndex] = useState(null);
+ 
+    const [currentForm, setCurrentForm] = useState("login");
+ 
+    const [emailSent, setEmailSent] = useState(false);
+    const [forgotEmail, setForgotEmail] = useState("");
+ 
+    // chat gpt used
+    const isValidEmail = (email) => {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return regex.test(email.trim());
+    }
+ 
+ 
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setPreviousIndex(currentIndex);
+ 
+            let randomIndex;
+            do {
+                randomIndex = Math.floor(Math.random() * movies.length);
+            } while (randomIndex === currentIndex && movies.length > 1);
+ 
+            setCurrentIndex(randomIndex);
+        }, 3000);
+        return () => clearInterval(timer);
+    }, [currentIndex]);
+    return (
+        // full div screen...
+ 
         <div className="flex w-full h-screen overflow-hidden">
-            {/* left side of screen */}
-            <div className="w-[50%] bg-white bg-gradient-to-br from-slate-900 to-slate-800">
-                {/* In left side movie box */}
-                <div className="relative flex items-center justify-center h-full">
-                    {/* movie animation box */}
-                    <div>
-                        <img className="object-cover w-full h-[600px] rounded-2xl relative max-w-md"
-                        src="https://i.pinimg.com/736x/da/bb/d0/dabbd0d713372ba31df6902861f392e2.jpg" alt="" />
+ 
+            {/* left side of screen... */}
+ 
+            <div className="w-[50%] bg-white bg-gradient-to-br from-slate-900 to-slate-800 relative">
+ 
+                <div className="w-[50%] bg-gradient-to-br from-slate-900 to-slate-800">
+ 
+                    {/* Logo div */}
+ 
+                    <div className="absolute left-4 top-4">
+                        <h2 className="text-3xl font-bold text-white">🎬 ReviewHub</h2>
                     </div>
-                 </div>
-            </div>
-
-            {/* right side of the screen */}
-            <div className="w-[50%] bg-black flex items-center justify-center">
-                {/* right login box */}
-                <div className="">
-                    <h1 className="mb-2 text-4xl font-bold text-white">Welcome Back!</h1>
-                    <p className="mb-8 text-sm text-slate-400"> Login to continue to review movies</p>
-                    <input className="w-full p-3 mb-4 text-white border rounded-lg bg-slate-900 border-slate-700" 
-                    type="email" placeholder="Enter your email here..." onChange={(e) => setEmail(e.target.value)} value={email} />
-
-                    <input className="w-full p-3 mb-4 text-white border rounded-lg bg-slate-900 border-slate-700" 
-                    type="password" placeholder="Enter your password here..." onChange={(e) => setPassword(e.target.value)} value={password} /> 
-                    <button className="w-full py-3 mb-6 font-semibold text-white bg-blue-600 rounded-lg ">Login</button>
-                  <p className="text-sm text-center text-slate-400">Don't have an account? Signup</p>
-                    
-
+ 
+                    {/*  */}
+ 
+                    {/* let randomBgImageStart = movies[Math.trunc(Math.random() * movies.length)] ;
+                    (randomBgImageStart + 15) > (movies.length) ? ( randomBgImageStart -= 15): (randomBgImageStart += 0);                    let randomBgImageEnd = randomBgImageStart + 15; */}
+ 
+                    <div className="absolute top-0 left-0 w-full h-full pointer-events-none opacity-10">
+                        <div className="grid grid-cols-3 gap-4 p-4">
+                            {movies.slice(20, 35).map((movie, index) => (
+                                <img src={movie.poster} key={index}
+                                    className="object-cover w-full h-32 rounded-lg"
+                                />
+                            ))}
+                        </div>
+                    </div>
                 </div>
-
+ 
+                {/* In left side movie box */}
+ 
+                <div className="relative flex items-center justify-center h-full">
+ 
+                    {/* movie animation box */}
+ 
+ <div className="relative w-full max-w-md h-[600px]">
+   {movies.map((movie, index) => (
+   <div key={movie.id} className={`absolute inset-0 transition-all   duration-1000 ${index === currentIndex ?
+    "opacity-100 translate-y-0" :
+       index === previousIndex ?
+      "opacity-0 -translate-y-full" :
+        "opacity-0 translate-y-full"}`}>
+        <img src={movie.poster} alt=""
+       className="object-cover w-full h-full rounded-2xl" />
+      <div className="absolute bottom-0 left-0 right-0 px-6 py-4 rounded-b-2xl bg-gradient-to-t from-black to-transparent">
+                                    <h3 className="text-2xl font-bold text-white">{movie.name}</h3>
+                                </div>
+ 	                           </div>
+                        ))}
+                    </div>
+                </div>
             </div>
-                
+ 
+            {/* right side of screen... */}
+ 
+            <div className="w-[50%] bg-black flex items-center justify-center">
+ 
+                {/* right login box */}
+                <div className="relative w-full max-w-md overflow-hidden min-h-[500px]">
+ 
+                    <div className={`form-animate ${currentForm === "login" ? "form-animate-in" : "form-animate-out"}`}>
+ 
+                        {/* Login page */}
+ 
+                        <div className="w-full max-w-md p-8">
+                            <h1 className="form-h1">Welcome Back!</h1>
+                            <p className="form-p1">Login to continue to review movies</p>
+ 
+                            <input className="form-input"
+                                type="email" placeholder="Enter your email here..."
+                                onChange={(e) => setEmail(e.target.value)} value={email} />
+ 
+                            <input className="form-input"
+                                type="password" placeholder="Enter your password here...."
+                                onChange={(e) => setPassword(e.target.value)} value={password} />
+                            <div className="flex justify-end mb-4">
+ 
+                                <a href="#" onClick={() => setCurrentForm("forgot")}
+                                    className="form-a">
+                                    Forget Password?</a>
+                            </div>
+ 
+                            <button className="form-btn">Login</button>
+ 
+                            <p className="form-p2">Don't have an account?{" "}
+                                <a href="#" onClick={() => setCurrentForm("signup")}
+                                    className="form-a">
+                                    Signup for free</a>
+                            </p>
+                        </div>
+                    </div>
+ 
+                    {/* Signup form */}
+ 
+                    <div className={`form-animate ${currentForm === "signup" ? "form-animate-in" : "form-animate-out"}`}>
+ 
+                        <div className="w-full max-w-md p-8">
+                            <h1 className="form-h1">Create Account</h1>
+                            <p className="form-p1">Signing up to start reviewing movies</p>
+ 
+                            <input className="form-input"
+                                type="text" placeholder="Enter your name here..." />
+ 
+                            <input className="form-input"
+                                type="email" placeholder="Enter your email here..." />
+ 
+                            <input className="form-input"
+                                type="password" placeholder="Create password..." />
+ 
+                            <button className="form-btn">Sign Up</button>
+ 
+                            <p className="form-p2">Already have an account?{" "}
+                                <a href="#" onClickCapture={() => setCurrentForm("login")}
+                                    className="form-a">
+                                    Login</a>
+                            </p>
+                        </div>
+                    </div>
+ 
+                    {/* forgot form */}
+ 
+                    <div className={`form-animate ${currentForm === "forgot" ? "form-animate-in" : "form-animate-out"}`}>
+ 
+                        <div className="w-full max-w-md p-8">
+                            <h1 className="form-h1">Forgot Password?</h1>
+ 
+                            {emailSent && (
+                                <div className="p-4 mb-4 text-green-400 bg-green-900 rounded-lg">
+                                    Email sent successfully...
+                                </div>
+                            )}
+                            {emailSent || (
+                                <>
+                                    <p className="form-p1">Enter your email to reset password</p>
+ 
+                                    <input className="form-input" type="email" placeholder="Enter your email here..."
+                                        onChange={(e) => setForgotEmail(e.target.value)} value={forgotEmail} />
+ 
+                                    <button className="form-btn disabled:cursor-not-allowed hover:disabled:bg-red-600 hover:disabled:scale-100" onClick={() => setEmailSent(true)}
+                                            disabled={!isValidEmail(forgotEmail)}>Reset password</button>
+ 
+                                </>
+                            )}
+                            <p className="form-p2">Remember Password?{" "}
+                                <a href="#" onClick={() => setCurrentForm("login")}
+                                    className="form-a">Back to Login</a>
+                            </p>
+                        </div>
+                    </div>
+ 
+                </div>
+            </div>
         </div>
-
     );
-
 }
+
+
+ 
