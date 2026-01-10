6229a8a763321ac04c0428d3e5c051f76a53e08a
Jyoti Rani <jyoti.purav09@gmail.com>
2026-01-10 20:28:48 +0530

    mapping the movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
-import { Film,Apple } from "lucide-react"
-
+import { Film } from "lucide-react"
+import { movies } from "../data/Movies";
 
 export default function Home() {
+    // console.log(`Movies: ${JSON.stringify(movies)}`)
     return (
         <div className="min-h-screen bg-black">
 
             {/* nav area */}
 
             <nav className="fixed top-0 left-0 right-0 p-4 text-white bg-slate-900">
-                <h1>🎬 MovieHub</h1>
+                <h1>🎬 ReviewHub</h1>
             </nav>
 
             {/* main area */}
@@ -32,23 +33,20 @@ export default function Home() {
                 {/* home explore area */}
 
                 <div className="p-4">
-                    <h2 className="home-h1"><Film /><Apple/> Explore</h2>
+                    <h2 className="home-h1"><Film /> Explore</h2>
 
                     {/* movie grid area */}
 
                     <div className="grid grid-cols-5 gap-4">
-                        
-                        <div className="rounded-lg bg-slate-800">
-                            <img className="w-full rounded-t-lg"
-                            src="https://i.pinimg.com/1200x/1c/39/1a/1c391a10d6cc573da460758d31821602.jpg" alt="" />
-                            <h3 className="p-2 text-white">Inception</h3>
-                        </div>
 
-                        <div className="rounded-lg bg-slate-800">
-                            <img className="w-full rounded-t-lg"
-                            src="https://i.pinimg.com/736x/3e/1f/66/3e1f66f76ffe17e6cb6f30a2c08a6c3d.jpg" alt="" />
-                            <h3 className="p-2 text-white">Interstellar</h3>
-                        </div>
+                        {movies.map((movie) => (
+                            <div className="rounded-lg bg-slate-800 " key={movie.id}>
+                                <img className="object-cover w-full rounded-t-lg h-[400px]"
+
+                                    src={movie.poster} alt="" />
+                                <h3 className="p-2 text-white">{movie.name}</h3>
+                            </div>
+                        ))}
                     </div>
                 </div>
 
